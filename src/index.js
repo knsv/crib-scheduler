be2221870f8ae104b304820dcc8013491790dd4e
Knut Sveidqvist <knut@sveido.com>
2016-04-17 10:39:56 +0200

    Process startup modifed, test of documentation generation.

--- src/index.js
+++ src/index.js
@@ -5,14 +5,16 @@ var cronJob = require('cron').CronJob;
 var cribMq = require('crib-mq');
 
 var buss = cribMq.register('crib-scheduler', 'http://127.0.0.1:8900');
-console.log('Starting Scheduling service');
+console.log('Starting Scheduling service',process.cwd());
 module.exports = function(config){
     config.scheduler.forEach(function (cronItem) {
-        //log.trace('Setting up CRON event: ' + cronItem.id + 'with date str: ' + cronItem.date);
+        console.log('Setting up CRON event: ' + cronItem.id + 'with date str: ' + cronItem.date);
         var job = new cronJob(cronItem.date, function () {
-            //console.log('Cron triggering event: ' + cronItem.id);
+            console.log('Cron triggering event: ' + cronItem.id);
             buss.emit(cronItem.id, cronItem.args);
         }, null, true);
     });
 };
 
+var conf = require(process.cwd()+'/crib.conf.js');
+module.exports(conf);
